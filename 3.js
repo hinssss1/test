153b8c8741611ffed12f6f9b3f6ab5626d444a25
hinssss1 <907034097@qq.com>
2024-07-09 15:11:01 +0800

    hins

--- 3.js
+++ 3.js
@@ -1,30 +1,23 @@
 /**
 
 [rewrite_local]
-^https?:\/\/(userapi|yun)\.feidee\.net\/(v1\/(profile\/basic_info|profile)|cab-periodic-activity-ws\/terminal\/v2\/monthly-card\/status|cab-vip-ws\/terminal\/v1\/vip-users=show-info) url script-response-body https://raw.githubusercontent.com/hinssss1/test/master/3.js
+^https?:\/\/app\.zhongyi108\.com\/api\/user\/getUserInfoNew url script-response-body https://raw.githubusercontent.com/hinssss1/test/master/3.js
 
 [mitm]
-hostname = userapi.feidee.net, yun.feidee.net
+hostname = app.zhongyi108.com
 
 */
 
-let obj = JSON.parse($response.body);
+var chxm1023 = JSON.parse($response.body);
 
-const vip1 = /cab-periodic-activity-ws\/terminal\/v2\/monthly-card\/status/;
-const vip2 = /cab-vip-ws\/terminal\/v1\/vip-users=show-info/;
-const vip3 = /v1\/profile\/basic_info/;
-const vip4 = /v1\/profile/;
+chxm1023.data = {
+  "end_vip_time": "2099-12-31 23:59:59",
+  "is_svip": 1,
+  "tui_bean": 99999,
+  "user_level": 10,
+  "save_money": 88888,
+  "end_svip_time": "2099-12-31 23:59:59",
+  "is_vip": 1,
+};
 
-if (vip1.test($request.url)) {
-  obj.user.status = "1";
-} else if (vip2.test($request.url)) {
-  obj.vip_status = "1";
-} else if (vip3.test($request.url)) {
-  obj.is_vip = true;
-} else if (vip4.test($request.url)) {
-    obj.is_vip = true;
-}
-
-$done({
-  body: JSON.stringify(obj)
-});
+$done({body : JSON.stringify(chxm1023)});
