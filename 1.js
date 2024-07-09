5ab220e02554bf581fd1080e3c6963934f9c1c2f
hinssss1 <907034097@qq.com>
2024-07-09 10:26:50 +0800

    hins

--- 1.js
+++ 1.js
@@ -1,13 +1,16 @@
+/**
+
 [rewrite_local]
 ^https:\/\/miniapp\.guniuniu\.com\/api\/mini-app\/user$ url script-response-body https://raw.githubusercontent.com/hinssss1/test/master/1.js
 
 [mitm]
 hostname = miniapp.guniuniu.com
 
+*/
 
 let obj = JSON.parse($response.body);
 obj.data.nickName = "zkj测试";
-obj.data.vipTime = "2099-09-09 02:20:13";
+obj.data.vipTime = "2098-09-09 02:20:13";
 //obj.data.svipTime = "2099-09-09 02:20:13";
 $done({
 
