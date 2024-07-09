bb42c3735d99af65367c5794c18ec8a42b79ba93
hinssss1 <907034097@qq.com>
2024-07-09 15:21:17 +0800

    hins

--- 3.js
+++ 3.js
@@ -14,10 +14,11 @@ chxm1023.data = {
   "end_vip_time": "2099-12-31 23:59:59",
   "is_svip": 1,
   "tui_bean": 99999,
-  "user_level": 10,
+  "user_level": 9,
   "save_money": 88888,
   "end_svip_time": "2099-12-31 23:59:59",
   "is_vip": 1,
+  "is_teacher": 1
 };
 
 $done({body : JSON.stringify(chxm1023)});
