82fad0ba849e85c942f7735a8765f9cf2b9837a5
hinssss1 <907034097@qq.com>
2024-07-09 16:22:28 +0800

    hins

--- 3.js
+++ 3.js
@@ -1,15 +1,10 @@
 /*
-
 [rewrite_local]
-
 https://app.zhongyi108.com/api/(user/getUcenterData|course/getDetail|user/getUserInfoNew) url script-response-body https://raw.githubusercontent.com/hinssss1/test/master/3.js
 
 [mitm]
-
 hostname = app.zhongyi108.com
-
 */
-
 if (typeof $response !== 'undefined' && $response.body) {
     let body = JSON.parse($response.body);
 
@@ -22,19 +17,35 @@ if (typeof $response !== 'undefined' && $response.body) {
                     switch (key) {
                         case 'is_vip':
                         case 'is_svip':
-                        case 'is_teacher':
-                        case 'is_follow':
+                        case 'is_buy':
+                        case 'is_free':
+                        case 'vip':
+                        case 'is_boutique':
+                        case 'is_try':
+                        case 'is_cash_coupon':
+                        case 'is_collect':
+                        case 'is_can_sale':
                             obj[key] = 1;
                             break;
-                        case 'end_vip_time':
-                        case 'end_svip_time':
-                            obj[key] = '2099-12-31 23:59:59';
+                        case 'vip_expire_time':
+                        case 'expire_time':
+                            obj[key] = '2099-09-09 09:09:09';
                             break;
-                        case 'tui_bean':
-                            obj[key] = 9999;
+                        case 'vip_grade':
+                            obj[key] = 'vip';
                             break;
                         case 'user_level':
+                        case 'level':
                             obj[key] = 9;
+                            break;
+                        case 'courseType':
+                            obj[key] = 'free';
+                            break;
+                        case 'vip_price':
+                        case 'price':
+                        case 'seckill_price':
+                        case 'line_price':
+                            obj[key] = '0.00';
                             break;
                     }
                 }
@@ -42,8 +53,26 @@ if (typeof $response !== 'undefined' && $response.body) {
         }
     }
 
-    modifyObject(body);
+    if (body.data) {
+        modifyObject(body.data);
+
+        // 特殊处理 play_log
+        if (!body.data.play_log) {
+            body.data.play_log = {
+                is_finished: 1,
+                progress: 100
+            };
+        }
+
+        // 特殊处理 course_list
+        if (body.data.course_list && Array.isArray(body.data.course_list)) {
+            body.data.course_list.forEach(course => {
+                course.is_try = 1;
+            });
+        }
+    }
+
     $response.body = JSON.stringify(body);
 }
 
-$done({ body: $response.body });
+$done({body: $response.body});
