857c4c0447a28035c2d5470cc6c98740da1b0fcd
hinssss1 <907034097@qq.com>
2024-07-09 14:13:09 +0800

    hins

--- 3.js
+++ 3.js
@@ -1,10 +1,10 @@
 /**
 
 [rewrite_local]
-^https?:\/\/yun\.feidee\.net\/(cab-periodic-activity-ws\/terminal\/v2\/monthly-card\/status|cab-vip-ws\/terminal\/v1\/vip-users=show-info) url script-response-body https://raw.githubusercontent.com/hinssss1/test/master/3.js
+^https?:\/\/(userapi|yun)\.feidee\.net\/(v1\/(profile\/basic_info|profile)|cab-periodic-activity-ws\/terminal\/v2\/monthly-card\/status|cab-vip-ws\/terminal\/v1\/vip-users=show-info) url script-response-body https://raw.githubusercontent.com/hinssss1/test/master/3.js
 
 [mitm]
-hostname = yun.feidee.net
+hostname = userapi.feidee.net, yun.feidee.net
 
 */
 
@@ -12,11 +12,17 @@ let obj = JSON.parse($response.body);
 
 const vip1 = /cab-periodic-activity-ws\/terminal\/v2\/monthly-card\/status/;
 const vip2 = /cab-vip-ws\/terminal\/v1\/vip-users=show-info/;
+const vip3 = /v1\/profile\/basic_info/;
+const vip4 = /v1\/profile/;
 
 if (vip1.test($request.url)) {
   obj.user.status = "1";
 } else if (vip2.test($request.url)) {
-  obj.vip_status = "1"; 
+  obj.vip_status = "1";
+} else if (vip3.test($request.url)) {
+  obj.isvip = true;
+} else if (vip4.test($request.url)) {
+    obj.is_vip = true;
 }
 
 $done({
