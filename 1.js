aa9a77f42998da279b08741367faf3114d151753
hinssss1 <907034097@qq.com>
2024-07-09 10:05:07 +0800

    hins

--- 1.js
+++ 1.js
@@ -1,3 +1,10 @@
+[rewrite_local]
+^https:\/\/miniapp\.guniuniu\.com\/api\/mini-app\/user$ url script-response-body local-modifyResponse.js
+
+[mitm]
+hostname = miniapp.guniuniu.com
+
+
 let obj = JSON.parse($response.body);
 obj.data.nickName = "Baby";
 obj.data.vipTime = "2099-09-09 02:20:13";
