118b25f0c47c53b41481a63fa9ece1751ffb4fa1
hinssss1 <907034097@qq.com>
2024-07-09 13:59:53 +0800

    hins

--- 3.js
+++ 3.js
@@ -13,15 +13,12 @@ let obj = JSON.parse($response.body);
 const vip1 = /cab-periodic-activity-ws\/terminal\/v2\/monthly-card\/status/;
 const vip2 = /cab-vip-ws\/terminal\/v1\/vip-users=show-info/;
 
-
-if(vip1.test($request.url)){
-  obj.user.ststus = "1"
-}
-
-if(vip2.test($request.url)){
-  obj.vip_ststus = "1"
+if (vip1.test($request.url)) {
+  obj.user.status = "1";
+} else if (vip2.test($request.url)) {
+  obj.vip_status = "1"; 
 }
 
 $done({
-body: JSON.stringify(obj)
+  body: JSON.stringify(obj)
 });
