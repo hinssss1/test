70427f529c35c5760f5ff32f3e4cb2bba86780af
hinssss1 <907034097@qq.com>
2024-07-10 11:58:15 +0800

    hins

--- 3.js
+++ 3.js
@@ -1,78 +1,42 @@
-/*
-[rewrite_local]
-https://app.zhongyi108.com/api/(user/getUcenterData|course/getDetail|user/getUserInfoNew) url script-response-body https://raw.githubusercontent.com/hinssss1/test/master/3.js
+let obj = JSON.parse($response.body);
 
-[mitm]
-hostname = app.zhongyi108.com
-*/
-if (typeof $response !== 'undefined' && $response.body) {
-    let body = JSON.parse($response.body);
+const userGetUcenterData = /\/api\/user\/getUcenterData/;
+const courseGetDetail = /\/api\/course\/getDetail/;
+const userGetUserInfoNew = /\/api\/user\/getUserInfoNew/;
+const mtim = /\/api\/mtim\/index/;
+const local = /\/api\/local\/index/;
 
-    function modifyObject(obj) {
-        for (let key in obj) {
-            if (obj.hasOwnProperty(key)) {
-                if (typeof obj[key] === 'object' && obj[key] !== null) {
-                    modifyObject(obj[key]);
-                } else {
-                    switch (key) {
-                        case 'is_vip':
-                        case 'is_svip':
-                        case 'is_buy':
-                        case 'is_free':
-                        case 'vip':
-                        case 'is_boutique':
-                        case 'is_try':
-                        case 'is_cash_coupon':
-                        case 'is_collect':
-                        case 'is_can_sale':
-                            obj[key] = 1;
-                            break;
-                        case 'vip_expire_time':
-                        case 'expire_time':
-                            obj[key] = '2099-09-09 09:09:09';
-                            break;
-                        case 'vip_grade':
-                            obj[key] = 'vip';
-                            break;
-                        case 'user_level':
-                        case 'level':
-                            obj[key] = 9;
-                            break;
-                        case 'courseType':
-                            obj[key] = 'free';
-                            break;
-                        case 'vip_price':
-                        case 'price':
-                        case 'seckill_price':
-                        case 'line_price':
-                            obj[key] = '0.00';
-                            break;
-                    }
-                }
-            }
-        }
+if (userGetUcenterData.test($request.url)) {
+    obj.data.is_svip = 1;
+    obj.data.is_teacher = 1;
+    obj.data.is_vip = 1;
+} else if (courseGetDetail.test($request.url)) {
+    // 处理课程详情
+    obj.data.teacher.is_vip = 1;
+    obj.data.teacher.user_level = 9;
+    obj.data.teacher.is_follow = 1;
+	obj.data.isboutique = 1
+	obj.data.virtual_buyers = 1
+} else if (userGetUserInfoNew.test($request.url)) {
+    obj.data.is_vip = 1;
+    obj.data.is_svip = 1;
+    obj.data.tui_bean = 9999;
+    obj.data.end_vip_time = '2099-12-31 23:59:59';
+    obj.data.end_svip_time = '2099-12-31 23:59:59';
+} else if (mtim.test($request.url)) {
+    // 处理MTIM相关数据
+    if (obj.data) {
+        obj.data.is_vip = 1;
+        obj.data.vip_level = 9;
+        obj.data.vip_expire_time = '2099-12-31 23:59:59';
     }
-
-    if (body.data) {
-        modifyObject(body.data);
-
-        // 特殊处理 play_log
-        if (!body.data.play_log) {
-            body.data.play_log = {
-                is_finished: 1,
-                progress: 100
-            };
-        }
-
-        // 特殊处理 course_list
-        if (body.data.course_list && Array.isArray(body.data.course_list)) {
-            body.data.course_list.forEach(course => {
-                course.is_try = 1;
-            });
-        }
+} else if (local.test($request.url)) {
+    // 处理local相关数据
+    if (obj.data) {
+        obj.data.is_vip = 1;
+        obj.data.vip_status = 1;
+        obj.data.vip_expire_time = '2099-12-31 23:59:59';
     }
-
-    $response.body = JSON.stringify(body);
 }
 
-$done({body: $response.body});
+$done({ body: JSON.stringify(obj) });
